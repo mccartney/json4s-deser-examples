fc7a606433d4fc1a6d99baf3a2dc345639635882
Greg Oledzki <goledzki@sumologic.com>
2020-05-21 22:59:15 +0200

    Reflector? Scala 2.12?

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,6 +2,7 @@ package example
 
 import org.json4s._
 import org.json4s.native.JsonMethods._
+import org.json4s.reflect.Reflector
 
 object Hello extends App {
 
@@ -17,6 +18,11 @@ object Hello extends App {
                    ) {
     def this(unrelated: Int, weird: Boolean) = this("John", "Doe", Friendliness("not much"))                   
  }
+
+  val k = classOf[Person]
+  val klass = Reflector.scalaTypeOf(k)
+  val descriptor = Reflector.describe(klass).asInstanceOf[reflect.ClassDescriptor]
+  System.out.println(descriptor.constructors.map(_.params.length.toString).mkString(","))
 
 
   val parsed = parse("""{"firstName": "Stephen", "lastName": "Falken"}""")
